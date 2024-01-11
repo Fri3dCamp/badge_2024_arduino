fba7dde4b705be80bef3739fe33ab39e5d0aa10e
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2024-01-11 20:51:43 +0100

    Platformio basic examples: serial port hello world & led blink

---
+++ examples/platformio basic examples/serial_port_hello_world/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(3000); 
+  Serial.println("Hello world");
+}
+
+void loop()
+{
+  Serial.println("Hello world again");
+  delay(1000);
+}
