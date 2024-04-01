d9c67a9af732af20069571e04a5dc1531caca9e8
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2024-04-01 10:07:57 +0200

    SPIFFS slideshow (#14)
    Co-authored-by: Bart Huylebroeck <bahoeman+git@gmail.com>

---
+++ examples/platformio basic examples/SPIFFS-slideshow/src/pins2024.h
@@ -0,0 +1,32 @@
+#define PIN_LEDS 12
+#define PIN_WS2812 12
+#define PIN_BUZZER 46
+#define PIN_IR_RECEIVER 11
+#define PIN_BLASTER 10
+
+//buttons:
+#define PIN_JOY_Y 3
+#define PIN_JOY_X 1
+#define PIN_A 39
+#define PIN_B 40
+#define PIN_X 38
+#define PIN_Y 41
+#define PIN_MENU 42
+#define PIN_START 0
+
+// SPI devices:
+#define SPI_MISO 8
+#define SPI_MOSI 6
+#define SPI_SCLK 7
+
+#define LCD_CS 5
+#define LCD_RS 4
+#define LCD_RSTn 48
+
+#define SDCARD_CS 14
+
+// I2C devices:
+#define PIN_I2C_SCL 18
+#define PIN_I2C_SDA 9
+#define ACCEL_ 0x6B
+#define PIN_SAO 13
