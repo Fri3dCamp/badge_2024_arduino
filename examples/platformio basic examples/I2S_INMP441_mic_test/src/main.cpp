2b02abd5e7c3614006a62f497cd78028b2f01450
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2024-07-22 11:32:05 +0200

    show mic on TFT

--- examples/platformio basic examples/I2S_INMP441_mic_test/src/main.cpp
+++ examples/platformio basic examples/I2S_INMP441_mic_test/src/main.cpp
@@ -1,7 +1,4 @@
 #include <Arduino.h>
-
-// Updated version of https://github.com/atomic14/esp32-i2s-mic-test/blob/main/i2s_mic_test/i2s_mic_test.ino
-
 #include <driver/i2s.h>
 
 // you shouldn't need to change these settings
@@ -42,19 +39,17 @@ i2s_pin_config_t i2s_mic_pins = {
     .data_in_num = I2S_MIC_SERIAL_DATA};
 
 
-void setup()
-{
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  delay(3000);
-  Serial.println("ESP32S3 - INMP441 - I2S microphone test");
+#include "TFT_eSPI.h"
 
-  Serial.print("I2S_MIC_SERIAL_CLOCK: ");
-  Serial.println(I2S_MIC_SERIAL_CLOCK);
-  Serial.print("I2S_MIC_LEFT_RIGHT_CLOCK: ");
-  Serial.println(I2S_MIC_LEFT_RIGHT_CLOCK);
-  Serial.print("I2S_MIC_SERIAL_DATA: ");
-  Serial.println(I2S_MIC_SERIAL_DATA);
+TFT_eSPI tft = TFT_eSPI();
+
+TFT_eSprite mic_sprite = TFT_eSprite(&tft);
+#define MIC_SPRITE_WIDTH 220
+#define MIC_SPRITE_HEIGHT 160
+
+void mic_sprite_setup()
+{
+  mic_sprite.createSprite(MIC_SPRITE_WIDTH, MIC_SPRITE_HEIGHT);
 
   // start up the I2S peripheral
   if (ESP_OK != i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL))
@@ -67,16 +62,67 @@ void setup()
   }
 }
 
-int32_t raw_samples[SAMPLE_BUFFER_SIZE];
-void loop()
+void mic_sprite_show()
 {
+  int32_t raw_samples[SAMPLE_BUFFER_SIZE];
+
+  mic_sprite.fillSprite(TFT_BLACK);
+  mic_sprite.drawRect(0,0,MIC_SPRITE_WIDTH, MIC_SPRITE_HEIGHT,TFT_WHITE );
+
   // read from the I2S device
   size_t bytes_read = 0;
   i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
-  int samples_read = bytes_read / sizeof(int32_t);
+  int num_samples = bytes_read / sizeof(int32_t);
   // dump the samples out to the serial channel.
-  for (int i = 0; i < samples_read; i++)
+
+  if (num_samples > 100)
   {
-    Serial.printf("%15ld\n", raw_samples[i] >> 19);
+    const float x_step = (float)MIC_SPRITE_WIDTH / (float)num_samples;
+    const float height2 = (float)MIC_SPRITE_HEIGHT / 2;
+
+    float sample_x = 0;
+    float prev_sample_y = 0;
+    for (int i = 4; i < num_samples; i += 4)
+    {
+      float sample_y = (float)(raw_samples[i] >> 19);
+      if (sample_y > height2) {
+        sample_y = height2;
+      }
+      if (sample_y < -height2) {
+        sample_y = -height2;
+      }
+      mic_sprite.drawLine(sample_x, height2 + prev_sample_y, sample_x + x_step * 4, height2 + sample_y, 0xfff);
+      prev_sample_y = sample_y;
+      sample_x += x_step * 4;
+    }
+    mic_sprite.pushSprite(40, 60);
   }
+}
+
+void setup_display()
+{
+  tft.init();
+  tft.writecommand(TFT_MADCTL);
+  tft.writedata(TFT_MAD_BGR | TFT_MAD_MV); //exchange red and blue bytes, and mirror x-coordinates
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(tft.width()/2-90, 20);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.println("Fri3d Mic test");
+}
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(3000);
+  Serial.println("Fri3d Badge 2024 - INMP441 - I2S microphone test");
+
+  setup_display();
+  mic_sprite_setup();
+}
+
+void loop()
+{
+  mic_sprite_show();
 }
