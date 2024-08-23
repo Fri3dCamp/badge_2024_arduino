39863ae76cb480dfce14dbcb204555322c21cde2
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2024-08-23 21:01:15 +0200

    Add buttons example with TFT display
    - Platformio : add TFT display to buttons example
    - Arduino IDE: add buttons example

---
+++ examples/platformio basic examples/buttons/src/Fri3dBadge_pins.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define PIN_I2C_SDA       9
+#define PIN_I2C_SCL      18
+#define PIN_WS2812       12
+#define PIN_LED          21
+#define PIN_IR_RECEIVER  11 
+#define PIN_BLASTER      10
+#define PIN_BUZZER       46
+#define PIN_BATTERY      13
+
+#define PIN_SDCARD_CS    14
+
+#define PIN_JOY_X         1
+#define PIN_JOY_Y         3
+
+#define PIN_A            39
+#define PIN_B            40
+#define PIN_X            38
+#define PIN_Y            41
+#define PIN_MENU         45
+#define PIN_START         0
+
+#define PIN_AUX          42
+
+#define CHANNEL_BUZZER 0
